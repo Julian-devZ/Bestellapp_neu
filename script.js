4e3a6f38ae63e27e091e1656c85240060c85793f
Julian-devZ <julian-zervas@gmx.de>
2026-03-27 10:56:59 +0100

    add category andb render dishes to categorys

--- script.js
+++ script.js
@@ -1,58 +1,72 @@
-let myFood = [
-  {
-    name: "Spaghetti Bolognese",
-    description: "Spaghetti mit einer herzhaften Rindfleisch-Tomatensauce",
-    price: 8.5,
-    id: 1,
-  },
+let myFood = {
+  Pasta: [
+    {
+      name: "Spaghetti Bolognese",
+      description: "Spaghetti mit einer herzhaften Rindfleisch-Tomatensauce",
+      price: 8.5,
+      amount: 0,
+      image: "assets/images/fresh-pasta.jpg",
+    },
 
-  {
-    name: "Penne Arrabbiata",
-    description: "Penne mit scharfer Tomatensauce und Chili",
-    price: 7.5,
-    id: 1,
-  },
+    {
+      name: "Penne Arrabbiata",
+      description: "Penne mit scharfer Tomatensauce und Chili",
+      price: 7.5,
+      amount: 0,
+      image: "assets/images/fresh-pasta.jpg",
+    },
+  ],
 
-  {
-    name: " Pizza Margherita",
-    description: "Pizza mit Tomatensauce, Mozzarella und frischem Basilikum",
-    price: 9.0,
-    id: 1,
-  },
+  Pizza: [
+    {
+      name: " Pizza Margherita",
+      description: "Pizza mit Tomatensauce, Mozzarella und frischem Basilikum",
+      price: 9.0,
+      amount: 0,
+      image: "assets/images/pizza.jpg",
+    },
 
-  {
-    name: " Pizza Salami",
-    description: "Pizza mit Tomatensauce, Mozzarella und Salami",
-    price: 10.5,
-    id: 1,
-  },
-
-  {
-    name: "Caesar Salat",
-    description: "Römischer Salat mit Caesar-Dressing, Croutons und Parmesan",
-    price: 6.5,
-    id: 1,
-  },
-  {
-    name: "Gemischter Salat",
-    description:
-      "Römischer Salat mit verschiedenen Sorten, Croutons und Parmesan",
-    price: 6.5,
-    id: 1,
-  },
-  {
-    name: "Pollo Salat",
-    description: "Römischer Salat mit Hähnchenstreifen, Croutons und Parmesan",
-    price: 6.5,
-    id: 1,
-  },
-  {
-    name: "Grüner Salat",
-    description: "Frischer gemischter Salat mit verschiedenen Gemüse",
-    price: 5.5,
-    id: 1,
-  },
-];
+    {
+      name: " Pizza Salami",
+      description: "Pizza mit Tomatensauce, Mozzarella und Salami",
+      price: 10.5,
+      amount: 0,
+      image: "assets/images/pizza.jpg",
+    },
+  ],
+  Salat: [
+    {
+      name: "Caesar Salat",
+      description: "Römischer Salat mit Caesar-Dressing, Croutons und Parmesan",
+      price: 6.5,
+      amount: 0,
+      image: "assets/images/salad.jpg",
+    },
+    {
+      name: "Gemischter Salat",
+      description:
+        "Römischer Salat mit verschiedenen Sorten, Croutons und Parmesan",
+      price: 6.5,
+      amount: 0,
+      image: "assets/images/salad.jpg",
+    },
+    {
+      name: "Pollo Salat",
+      description:
+        "Römischer Salat mit Hähnchenstreifen, Croutons und Parmesan",
+      price: 6.5,
+      amount: 0,
+      image: "assets/images/salad.jpg",
+    },
+    {
+      name: "Grüner Salat",
+      description: "Frischer gemischter Salat mit verschiedenen Gemüse",
+      price: 5.5,
+      amount: 0,
+      image: "assets/images/salad.jpg",
+    },
+  ],
+};
 let myImages = [
   "assets/images/fresh-pasta.jpg",
   "assets/images/fresh-pasta.jpg",
@@ -69,35 +83,105 @@ let footerRef = document.getElementById("footer");
 let ContentRef = document.getElementById("content");
 let ContentRef2 = document.getElementById("content2");
 const basketRef = document.getElementById("basket");
+let overlayRef = document.getElementById("overlay");
 
 const img = document.getElementById("image");
 let myBasket = [];
+let deleleDishes = [];
 
-function createContent() {
-  console.log("start");
-  for (let indexFood = 0; indexFood < myFood.length; indexFood++) {
-    ContentRef.innerHTML += showContent(indexFood);
+function init() {
+  createBasket();
+  createOverlay();
+  //unvalidamount();
+}
+
+for (let category in myFood) {
+  // Eigener Container pro Kategorie
+  let categoryHTML = `<div class="category id="${category}">
+                        <h2 class= "border">${category}</h2>`;
+
+  // Nur die Gerichte dieser Kategorie
+  for (let item of myFood[category]) {
+    categoryHTML += `
+      <div class="border">
+        <h3>${item.name}</h3>
+        <p>${item.description}</p>
+        <p>${item.price} €</p><button onclick="addtoBasket()">+</button></p>
+         <img src="${item.image}" alt="${item.name}">
+      </div>
+    `;
+  }
+
+  categoryHTML += `</div>`;
+
+  // Kategorie separat ins HTML einfügen
+  ContentRef.innerHTML += categoryHTML;
+}
+//function createContent() {
+//for (let category in myFood) {
+//console.log("Kategorie:", category);
+
+//// for (let indexFood = 0; indexFood < myFood[category].length; indexFood++) {
+//let item = myFood[category][indexFood];
+//ContentRef.innerHTML += showContent(item);
+//}
+//}
+//}
+function createImages() {
+  for (let indexImages = 0; indexImages < myImages.length; indexImages++) {
+    ContentRef.innerHTML += showImages(indexImages);
   }
 }
 
-function createContent2() {
-  ContentRef2.innerHTML += showContent2();
-}
-function createBasket(indexFood) {
-  basketRef.innerHTML = " Warenkorb";
+function createBasket(item) {
+  basketRef.innerHTML = "";
   for (let indexBasket = 0; indexBasket < myBasket.length; indexBasket++) {
-    basketRef.innerHTML += showBasket(indexBasket, indexFood);
+    basketRef.innerHTML += showBasket(indexBasket, item);
   }
 }
-
 function addtoBasket(indexFood) {
   const FoodBasket = myFood[indexFood];
   myBasket.push(FoodBasket);
   console.log(myBasket);
   createBasket();
 }
-
-//<h1> ${(myBasket[i].Preis * myBasket[i].id).toFixed(i)} €</h1>
+function addtoDelete(indexFood) {
+  const deleteBasket = myFood[indexFood];
+  deleleDishes.push(deleteBasket);
+  console.log(myBasket);
+  createBasket();
+}
 function toggleBasket() {
   basketRef.classList.toggle("none");
 }
+function addamount(indexFood) {
+  myBasket[indexFood].amount++;
+  basketRef.innerHTML = "";
+  for (let indexBasket = 0; indexBasket < myBasket.length; indexBasket++) {
+    basketRef.innerHTML += updateBasket(indexBasket, indexFood);
+  }
+}
+function lessamount(indexFood) {
+  myBasket[indexFood].amount--;
+  basketRef.innerHTML = "";
+  for (let indexBasket = 0; indexBasket < myBasket.length; indexBasket++) {
+    basketRef.innerHTML += updateBasket2(indexBasket, indexFood);
+  }
+}
+function Openoverlay() {
+  overlayRef.classList.remove("none");
+  basketRef.classList.add("none");
+  setTimeout(() => {
+    overlayRef.classList.add("none");
+    setTimeout(() => overlayRef.close(), 500);
+  }, 4000);
+}
+function createOverlay() {
+  overlayRef.innerHTML += showOverlay();
+}
+//function unvalidamount(indexFood) {
+//if (myFood[indexFood].amount < 1) {
+//  console.log("Menge kann nicht weniger als 0 sein!");
+// myFood[indexFood].amount >= 0;
+//}
+//}
--- template.js
+++ template.js
@@ -1,36 +1,58 @@
-function showContent2() {
-  return `  <div><img src="assets/images/food_is_on_the_way.png" alt="Bestellungs unterwegs(Bild)"></div>
-  <p> <strong> IHRE BESTELLUNSG IST UNTERWEGS!</strong></p>
-    `;
-}
-function showContent(indexFood, imageIndex) {
-  return `
-   <section id="Pasta" class="border">
-   
-    <p>${myFood[indexFood].name}</p>
-     <p>${myFood[indexFood].price}€ <button onclick="addtoBasket(${indexFood})">+</button></p>
-      <p>${myFood[indexFood].description}</p>
-
-      <img id="image"src ="assets/images/fresh-pasta.jpg"/></img>
-</section>
-    `;
-}
-
-function showBasket(i) {
+function showBasket(indexFood, item) {
   return `
   
   <div class="items_basket">
   <div class="basket_font">
-<h2>${myBasket[i].name}</h2>
+<h2>${myBasket.item.name}</h2>
 <div class ="basket_price">
-<h2>Menge: ${myBasket[i].id}</h2> 
-<button>+</button>
- <button>-</button> <br>
-  <button>X</button>
+<h2>Menge:<button onclick="addamount(${indexFood})">+</button> ${item.amount}<button>-</button> <br></h2>
+<h2> ${(myBasket.item.price * myBasket.item.amount).toFixed(indexFood)} €</h2>
+  <button onclick="toggleBasket(),addtoDelete()">X</button>
+ </div>
+</div>
+<a onclick="Openoverlay()">Ihre Bestellung hier abschließen</a>
+`;
+}
+function updateBasket(indexFood) {
+  return `
+  
+  <div class="items_basket">
+  <div class="basket_font">
+<h2>${myBasket[indexFood].name}</h2>
+<div class ="basket_price">
+<h2>Menge:<button onclick=" addamount(${indexFood})">+</button> ${myFood[indexFood].amount}<button onclick="lessamount(${indexFood})">-</button> <br></h2>
+<h2> ${(myBasket[indexFood].price * myBasket[indexFood].amount).toFixed(indexFood)} €</h2>
+
+ 
+  <button onclick="toggleBasket()">X</button>
  </div>
 </div>
 
 `;
+}
+function updateBasket2(indexFood) {
+  return `
+  
+  <div class="items_basket">
+  <div class="basket_font">
+<h2>${myBasket[indexFood].name}</h2>
+<div class ="basket_price">
+<h2>Menge:<button onclick=" addamount(${indexFood})">+</button> ${myFood[indexFood].amount}<button onclick="lessamount(${indexFood}),unvalidamount(${indexFood})">-</button> <br></h2>
+<h2> ${(myBasket[indexFood].price * myBasket[indexFood].amount).toFixed(indexFood)} €</h2>
+
+ 
+  <button onclick="toggleBasket()">X</button>
+ </div>
+</div>
+
+`;
+}
+function showOverlay() {
+  return `
+
+    <div id ="overlay"><img src="assets/images/food_is_on_the_way.png" alt="food on the food_is_on_the_way">Ihre Bestellung ist unterwegs!</div>
+   
+    `;
 }
 
 //<h2> ${(myBasket[i].price * myBasket[i].id).toFixed(i)} €</h2>
